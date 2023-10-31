70b10d7eeb354c3abf15ea1cf5a38c4dec6cc834
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2023-10-31 15:45:12 +0530

    pattern-1

---
+++ pattern-1.js
@@ -0,0 +1,19 @@
+/* Javascript function to print pattern 
+    *		
+    **
+    ***
+    ****
+    *****
+*/
+
+function pattern(n) {
+    let str = "";
+    for(let i=0;i<=n;i++){
+        for(let j=0;j<i;j++) {
+            str += '*';
+        }
+    str += "\n";
+    }
+    return str;
+}
+console.log(pattern(5));
