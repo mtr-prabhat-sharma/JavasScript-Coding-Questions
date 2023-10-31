5d3434f0e800ead0156bb7c0300288370a94483c
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2023-10-31 15:38:26 +0530

    capitalizeFirstLetter-1

--- capitalizeFirstLetter.js
+++ capitalizeFirstLetter.js
@@ -1,8 +1,12 @@
 /* JavaScript function to capilatize the first letter of each word in a string*/
+
 function capitalizeFirstLetter(s) {
-    const words = s.split(' ');
+    const words = s.split(' '); // Split the string into array of words
+    /*It will iterate through all the words inside array and capitilalize the 
+    character at 0 index and then concat it with the rest of the word by remoivng 
+    the first letter which is smaller*/
     const cw = words.map((word) => {
-        return word.charAt(0).toUpperCase() + word.slice(1);
+        return word.charAt(0).toUpperCase() + word.slice(1); 
      })
     let res = cw.join(' ');
     return res;
