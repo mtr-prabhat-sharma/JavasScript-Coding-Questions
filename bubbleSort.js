d7e631829abdea5730e3b9fcb1f16ad5b14fd085
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2023-11-01 11:08:43 +0530

    sorting array elements using bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,14 @@
+function bubleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n; i++) {
+        for (j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+console.log(bubleSort([1, 4, 5, 3, 2]));
