ac3e89e9e509457092abad2b6569c6b9acb6412a
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2023-11-01 11:22:23 +0530

    added comments

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,7 @@
+/*Bubble sort algorithm is an algorithm that sorts an array by comparing two 
+adjacent elements and swapping them if they are not in the intended order. 
+Here order can be anything like increasing or decreasing. */
+
 function bubleSort(arr) {
     let n = arr.length;
     for (let i = 0; i < n; i++) {
@@ -12,3 +16,13 @@ function bubleSort(arr) {
     return arr;
 }
 console.log(bubleSort([1, 4, 5, 3, 2]));
+
+/**
+ * Worst Case and Average case time complexity 
+If the array is in reverse order then this condition is the worst case and 
+Its time complexity is O(n2).
+
+Best case time complexity
+If the array is already sorted then it is the best-case scenario and 
+its time complexity is O(n)
+ */
