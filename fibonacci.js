06bb8ba93e2d32cd9eccd368f0474836465ee53d
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2023-11-09 11:26:43 +0530

    fibonacci series to print in single line

---
+++ fibonacci.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+    let fibArr = [0,1];
+    for(let i=2;i<n;i++) {
+        fibArr[i] = fibArr[i-1] + fibArr[i-2];
+    }
+    console.log(fibArr.join(','));
+    
+}
+fibonacci(7)
+
+/*
+Output: 0,1,1,2,3,5,8
+*/
