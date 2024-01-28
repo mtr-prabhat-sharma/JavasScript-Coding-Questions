f583b8848f45955737d4ad39bd4a3f7d61271ad3
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2024-01-28 17:22:19 +0530

    countOccurenceofCharacters

---
+++ countOccurenceofCharacters.js
@@ -0,0 +1,17 @@
+//This will count occurence of characters and prints result in string format
+function countOccurenceofCharacters(s) {
+    let str = s.split('').sort();
+    let resStr = "";
+    let count = 1;
+    for(let i=0;i<str.length;i++) {
+            if(str[i] === str[i+1]) {
+                count ++;
+                
+            } else {
+                resStr = resStr + str[i] + count;
+                count = 1;
+            }
+    }
+    console.log(resStr)
+}
+countOccurenceofCharacters('aabdcdcabdd')
