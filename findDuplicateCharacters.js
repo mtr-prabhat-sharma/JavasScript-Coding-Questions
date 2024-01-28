0bc5a349204d4d43cc3098bec60302a4c19e3b1d
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2024-01-28 16:31:52 +0530

    findDuplicateCharacters

---
+++ findDuplicateCharacters.js
@@ -0,0 +1,14 @@
+function findDuplicateCharacters(str) {
+    let charCount = {};
+    let duplicates = [];
+    for(let char of str) {
+        charCount[char] = charCount[char] !== undefined ? charCount[char] + 1 : 1;
+    }
+    for(let char in charCount){
+        if(charCount[char] > 1) {
+            duplicates.push(char);
+        }
+    }
+    return duplicates;
+}
+console.log(findDuplicateCharacters('rab ha tsha ra'));
