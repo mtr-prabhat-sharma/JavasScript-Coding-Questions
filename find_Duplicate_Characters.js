27aa94b1ca9609679354619afd84c3141f3dbd03
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2024-02-06 16:30:43 +0530

    new files

--- find_Duplicate_Characters.js
+++ find_Duplicate_Characters.js
@@ -2,6 +2,7 @@ function findDuplicateCharacters(str) {
     let charCount = {};
     let duplicates = [];
     for(let char of str) {
+        console.log(char)
         charCount[char] = charCount[char] !== undefined ? charCount[char] + 1 : 1;
     }
     for(let char in charCount){
--- valid_parenthesis.js
+++ valid_parenthesis.js
@@ -1,21 +1,3 @@
-// function demo() {
-//     var name = 'dasdas';
-// const obj = {
-//     name: 'prabhat',
-//     printName() {
-//         console.log(this.name);
-//     },
-//     printName1: () => {
-//         let name = 'rwewe';
-//         console.log(this.name);
-//     }
-
-// }
-
-// obj.printName();
-// obj.printName1();
-// }
-// demo();
 
 function isBalanced(str) {
     let arr = [];
