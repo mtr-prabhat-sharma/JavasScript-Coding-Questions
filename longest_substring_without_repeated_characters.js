abfc4b3524d5383844bc9df37dfd1a1240bc7b45
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2024-02-12 15:46:28 +0530

    longest substrng

--- longest_substring_without_repeated_characters.js
+++ longest_substring_without_repeated_characters.js
@@ -5,7 +5,7 @@ function lengthOfLongestSubstring(str) {
     let start = 0;
     let next = 0;
   
-    while (next < str.length) {
+    while (next < str.length) { 
       let char = str[next];
       if (seen[char]) {
         start = Math.max(start, seen[char]);
@@ -21,4 +21,37 @@ function lengthOfLongestSubstring(str) {
     console.log(longestStr);
   }
   
-  lengthOfLongestSubstring("abcdad");
+  lengthOfLongestSubstring("abcaad"); //Output: abc
+
+
+/**
+longest = 0;
+next = 0;
+start = 0;
+str.length = 6
+while -> 0 < 6 -> true
+    char = str[next] = str[0] = a;
+    1st if -> seen[a] -> false
+    2nd if -> longest < next - start + 1 = 0 < 0-0+1 = 0 < 1 -> true
+        longest = next - start + 1 = 0-0+1 = 1;
+        longestStr = str.slice(start, next + 1) = str.slice(0, 1) = ab;
+    seen[char] = next + 1 = 1;
+    next ++ -> next = 1;
+
+while -> 1 < 6 -> true
+    char = str[next] = str[1] = b;
+    1st if -> seen[b] -> false;
+    2nd if -> 1 < 1 - 0 + 1 = 1 < 2 -> true
+        longest = 1 - 0 + 1 = 2;
+        longestStr = str.slice(0, 2) = abc;
+    seen[char] = next + 1 = 1 + 1 = 2;
+    next++ -> next = 2;
+    
+while -> 2 < 6 -> true;
+    char = str[next] = str[2] = c;
+    1st if -> seen[c] -> false;
+    2nd if -> 2 < 2 - 0 + 1 = 2 < 3 -> true
+        longest = 2 - 0 + 1 = 1;
+        longestStr = str.slice(0, )
+
+*/
