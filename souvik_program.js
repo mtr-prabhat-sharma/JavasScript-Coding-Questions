42f44d174f3d4caebefafe32d6e2498dcb798605
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2024-04-06 15:23:59 +0530

    update

--- souvik_program.js
+++ souvik_program.js
@@ -21,3 +21,52 @@ function demo(arr1, m, arr2, n) {
     console.log(arr1.sort())
 }
 demo(nums1, m, nums2, n)
+
+// ************************************************************************ //
+
+//OTHER SOLUTION
+/**
+  Algo
+
+     Initialize three pointers:
+
+    i to point to the last non-zero element in nums1.
+    j to point to the last element in nums2.
+    k to point to the last index of the merged array.
+    Iterate over nums1 and nums2 from the end:
+
+    Compare the elements at nums1[i] and nums2[j].
+    Place the larger element at nums1[k].
+    Decrement k and the pointer corresponding to the larger element.
+    Repeat until either nums1 or nums2 is fully processed.
+
+    If there are remaining elements in nums2:
+    Place them in the remaining slots of nums1
+
+*/
+const nums1 = [1, 2, 3, 0, 0, 0, 0];
+const m = 3;
+const nums2 = [2, 5, 6, 7];
+const n = 4;
+
+let i = m - 1; // Index for nums1
+let j = n - 1; // Index for nums2
+let k = m + n - 1; // Index for merged array
+
+while( i >= 0 && j >= 0) {
+    if(nums1[i] > nums2[j]) {
+        nums1[k] = nums1[i];
+        i--;
+    }
+    else {
+        nums1[k] = nums2[j];
+        j--;
+    }
+    k--;
+}
+while( j >= 0 ) {
+    nums1[k] = nums2[j];
+    k--;
+    j--;
+}
+console.log(nums1);
