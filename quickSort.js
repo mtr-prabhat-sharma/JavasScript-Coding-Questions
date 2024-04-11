de58e896bfc6c37fa8082c68c8ff403decce5bc6
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2024-04-11 07:16:05 +0530

    updates

--- quickSort.js
+++ quickSort.js
@@ -1,9 +1,19 @@
 /*
- QuickSort is a widely used sorting algorithm that efficiently sorts an array or 
- list by partitioning it into smaller subarrays. 
+ QuickSort is a widely used sorting algorithm that efficiently sorts an 
+ array or list by partitioning it into smaller subarrays. 
  It was developed by Tony Hoare in 1960. 
- QuickSort is known for its average-case time complexity of O(n log n) and 
- its space efficiency.
+ QuickSort is known for its average-case time complexity of O(n log n) 
+ and its space efficiency.
+
+ ALGO:
+ 1. If the array has 0 or 1 elements, return the array (base case).
+ 2. Select a pivot element (usually the middle element).
+ 3. Partition the array into two sub-arrays:
+    - Elements less than the pivot (left sub-array).
+    - Elements greater than the pivot (right sub-array).
+ 4. Recursively apply quickSort on the left and right sub-arrays.
+ 5. Combine the sorted left sub-array, pivot element, and sorted right sub-array.
+ 6. Return the combined array as the result.
  
  */
 
@@ -13,13 +23,13 @@ function quickSort(arr) {
         return arr;
     }
     
-    const pivot = arr[0];
+    const pivot = arr[Math.floor(arr.length / 2)];
     const left = [];
     const right = [];
-    for(let i = 1;i<arr.length;i++) {
+    for(let i = 0;i<arr.length;i++) {
         if(arr[i] < pivot) {
             left.push(arr[i]);
-        } else {
+        } else if(arr[i] > pivot){
             right.push(arr[i]);
         }
     }
