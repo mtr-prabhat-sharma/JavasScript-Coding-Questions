193d089f15df75eea0711fcb4dba20ce1c3491c4
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2024-04-21 08:22:17 +0530

    updates

--- functionCurrying.js
+++ functionCurrying.js
@@ -7,7 +7,7 @@ function sum(a) {
 }
 console.log(sum(1)(2)(3)); //Output: 6
 
-
+/***********************************************************************************/
 function sum1(a) {
     return function (b) {
         if(b) {
@@ -18,3 +18,17 @@ function sum1(a) {
  }
 
 console.log(sum1(1)(2)(4)(5)(6)());
+
+/***********************************************************************************/
+ function sum(...args1) {
+    let total = args1.reduce((a,c) => a+c);
+    return function(...args) {
+        if(args.length) {
+            let s = args.reduce((a,c) => a+c);
+            return sum(s+total);
+        }
+        return total;
+    }
+}
+console.log(sum(3, 4,1)(5, 6, 7)(7, 8,9)()); //50
+/***********************************************************************************/
