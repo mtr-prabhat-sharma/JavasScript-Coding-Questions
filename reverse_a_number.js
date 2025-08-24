c233346a4102144a5567c4368ee27604143ee836
Prabhat Sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2025-08-24 22:56:06 +0530

    Update reverse_a_number.js

--- reverse_a_number.js
+++ reverse_a_number.js
@@ -9,3 +9,17 @@ function reverseNumber(num) {
    
 }
 console.log(reverseNumber(2456))
+
+******************************************************************
+// Without using loop (using recursion)
+   
+function reverseNumber(num) {
+   let rev = '';
+   if(num % 10 === num) {
+       return num;
+   }
+   rev = (num % 10).toString() + reverseNumber(Math.floor(num / 10));
+   return rev;
+   
+}
+console.log(reverseNumber(2465))
