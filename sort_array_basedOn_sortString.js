b885e5705977b1cf854d8a591b6431c1c4e87508
Prabhat Sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2025-08-26 11:27:17 +0530

    Update sort_array_basedOn_sortString.js

--- sort_array_basedOn_sortString.js
+++ sort_array_basedOn_sortString.js
@@ -12,4 +12,15 @@ function sortArray(arr, sortString) {
 }
 const arr = ['Ashish', 'Shivam','Payal','Mehak'];
 const sortString = 'MAPS';
-console.log(sortArray(arr, sortString))
+console.log(sortArray(arr, sortString)) //[ 'Mehak', 'Ashish', 'Payal', 'Shivam' ]
+
+// Using sort function
+function demo(arr, sortString) {
+    let res = arr.sort((a,b) => {
+        return sortString.indexOf(a[0]) - sortString.indexOf(b[0])
+    })
+    return res;
+}
+const arr = ['Ashish', 'Shivam','Payal','Mehak'];
+const sortString = 'MAPS';
+console.log(demo(arr, sortString)) // [ 'Mehak', 'Ashish', 'Payal', 'Shivam' ]
