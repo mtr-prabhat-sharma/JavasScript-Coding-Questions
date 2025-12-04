8c72e8ca7b9e1eb60b15d55daac26e806eea6f9d
Prabhat Sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2025-12-04 14:49:24 +0530

    Update count_maxCount_number.js

--- count_maxCount_number.js
+++ count_maxCount_number.js
@@ -1,1 +1,18 @@
-....
+let arr = [4, 3, 4, 3, 2, 4, 1, 5, 4, 4]; 
+
+function findMaxFrequency(arr) { 
+   let maxCount; 
+   let res = {}; 
+   for (num of arr) { 
+      if (res[num]) { 
+         res[num]++; 
+      } else { 
+         res[num] = 1; 
+      } 
+   } 
+   return Math.max(...Object.values(res)); 
+} 
+
+console.log(findMaxFrequency(arr))
+
+***********************************†***********
