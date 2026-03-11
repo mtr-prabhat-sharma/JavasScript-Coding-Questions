10486ef7f02e222b604ea2736d506a1ebd361663
Prabhat Sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2026-03-11 15:26:18 +0530

    Update reverse_a_number.js

--- reverse_a_number.js
+++ reverse_a_number.js
@@ -2,7 +2,7 @@ function reverseNumber(num) {
    let rev = '';
    while(num > 0) {
        let last = num % 10;
-       rev = rev + last;
+       rev = rev * 10 + last; //To keep rev a number instead of string
        num = Math.floor(num/10)
    }
    return rev;
